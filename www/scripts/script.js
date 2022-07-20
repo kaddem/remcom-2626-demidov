d7016bc3242e6ed2adf75ac8f354caff1c878ff9
Kirill Demidov <kademidov@gmail.com>
2022-07-20 14:48:16 +0300

    адаптив главной. Начали изучение js

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,58 @@
 console.log('script work!');
+
+
+let petsName = ['Fiona', 'Kesha', 'Robin', 'Dik'];
+
+console.log( petsName.length );
+
+let summ = 2 + 2 * 2;
+
+console.log(summ);
+
+let petName = 'Мэдвэд';
+
+let message = 'Привет,' + ' ' + petName;
+
+console.log(message);
+
+console.log( 9 - '5d');
+
+
+
+
+let a = 1;
+
+console.log(a);
+
+a++;
+
+console.log(a);
+
+a++;
+
+console.log(a);
+
+a--;
+
+console.log(a);
+
+
+let age = 17;
+
+if (age > 18) {
+  console.log('Гуляю всю ночь!');
+} else {
+  console.log('Бери соску иди спать');
+}
+
+
+
+let name = 'dsaf' / 3;
+
+console.log(name);
+
+if (name) {
+  console.log('Здравствуйте, ' + name);
+} else {
+  console.log('Здравствуйте, товарищ!');
+}
